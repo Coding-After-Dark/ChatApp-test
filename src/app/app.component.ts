c2d498905411e0c613fce1a9325a999eefefbac9
Bwarhness <nalar94@gmail.com>
2017-12-20 17:10:39 +0100

    Test chat
    sda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
+import { ChatService } from './chat.service';
+import * as io from 'socket.io-client';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+
+  chats: any;
+  joinned: boolean = false;
+  yolo:string[] = [];
+  newUser = { nickname: '', room: '' };
+  msgData = { room: '', nickname: '', message: '' };
+  socket = io('http://localhost:3001');
+  msg = "";
+  username = "Yellow penguin";
+  ngOnInit(){
+    this.chats = [];
+
+    this.socket.on('new-message', function (data) {
+        this.chats.push(data);
+    }.bind(this));
+  }
+  SendMessage(){
+    var res = {
+      name: this.username,
+      msg: this.msg
+    }
+    this.socket.emit('save-message',res)
+    this.msg = ""
+  }
+
 }
